c1dff06b913c7a22a93c88ef8c3b4f6096b45bba
AD7six <andydawson76@gmail.com>
2011-11-07 13:16:17 +0100

    add an example with a message

---
+++ examples/message.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../ProgressBar.php';
+
+$size = 100;
+
+echo ProgressBar::start($size, "Starting in about 5 seconds");
+usleep(5000000);
+
+ProgressBar::message('Go!');
+for ($i = 1; $i <= $size; $i++) {
+    if ($i % 20) {
+        echo ProgressBar::next();
+    } else {
+        echo ProgressBar::next(1, "made it to $i yay!");
+    }
+    usleep(100000);
+}
+    
+echo ProgressBar::finish();
