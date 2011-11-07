9afba7b35e888e6fb422771d90306b978f86b803
AD7six <andydawson76@gmail.com>
2011-11-07 13:18:14 +0100

    add an overrun example

---
+++ examples/overrun.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../ProgressBar.php';
+
+$size = 10;
+
+echo ProgressBar::start($size);
+
+for ($i = 1; $i <= $size * 10; $i++) {
+    if ($i === ($size + 1)) {
+        ProgressBar::message('Ooops, looks like there\'s more work to do');
+    }
+    echo ProgressBar::next();
+    usleep(100000);
+}
+    
+echo ProgressBar::finish();
